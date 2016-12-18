6f1be9bc3e7fc68c069e00043758f3d50e29f689
Nosthertus <nosthertus@gmail.com>
2016-12-18 17:32:49 -0400

    + expose-header handler
    *Added conditional function for expose-header, it follows the same structure as allow-headers
    
    References #2

--- test/helper.js
+++ test/helper.js
@@ -52,4 +52,30 @@ describe("parsing values through helper", () => {
 		});
 	});
 
+	describe("parse value for \"expose-headers\" in \"values\" object on either String or Array", () => {
+		it("should throw error when provided value is not valid", () => {
+			options.defaults["expose-headers"] = true;
+			
+			expect(() => {helper.parseOptions(options)})
+				.to.throw(`Default Header for "expose-headers" must be Array or String`);
+		});
+
+		it("should contain multiple headers as string when Array is passed", () => {
+			options.defaults["expose-headers"] = ["ORIGIN", "content-type"];
+
+			expect(helper.parseOptions(options).values)
+				.to.include.keys("expose-headers");
+
+			assert.isString(helper.parseOptions(options).values["expose-headers"]);
+		});
+
+		it("should have same provided value if provided value is String", () => {
+			options.defaults["expose-headers"] = "Content-Type";
+
+			expect(helper.parseOptions(options).values)
+				.to.include.keys("expose-headers");
+			
+			assert.isString(helper.parseOptions(options).values["expose-headers"]);
+		});
+	});
 });
