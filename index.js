2595e95ef797f40eaaa565e213db5f65e63a4116
Nosthertus <nosthertus@gmail.com>
2016-12-18 22:06:42 -0400

    + header response from provided CORS instance
    *Added response headers which are parsed and set as header response

--- index.js
+++ index.js
@@ -23,12 +23,28 @@ module.exports = options => {
 			// Get all methods for the registrered request url
 			var methods = router.getMethods($.url.pathname, $.app.routes);
 
-
+			/*
+			 * Set CORS headers in the response
+			 */
 			$.header("Access-Control-Allow-Methods", methods.join(", "));
 			$.header("Access-Control-Allow-Origin", module.options.values["allow-origin"]);
 			$.header("Access-Control-Max-Age", module.options.values["max-age"]);
 
-			// Set status 204 with empty body
+			// console.log(module.options.values);
+
+			if(module.options.values["allow-headers"]){
+				$.header("Access-Control-Allow-Headers", module.options.values["allow-headers"]);
+			}
+
+			if(module.options.values["expose-headers"]){
+				$.header("Access-Control-Expose-Headers", module.options.values["expose-headers"]);
+			}
+
+			if(module.options.values["allow-credentials"]){
+				$.header("Access-Control-Allow-Credentials", module.options.values["allow-credentials"]);
+			}
+
+			// Set status 204 with empty body when method is OPTIONS
 			if($.method == "OPTIONS"){
 				$.status(204);
 				$.end();
